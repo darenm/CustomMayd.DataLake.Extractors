2ea86767985a9ead7ecc0c3b7984e009a58d7897
Daren May <daren.may@gmail.com>
2019-01-29 08:43:57 -0700

    formatted

--- CustomMayd.DataLake.Extractors/FixedWidthExtractor.cs
+++ CustomMayd.DataLake.Extractors/FixedWidthExtractor.cs
@@ -4,22 +4,20 @@
 // https://blogs.msdn.microsoft.com/data_otaku/2016/10/27/a-fixed-width-extractor-for-azure-data-lake-analytics/'
 //
 
-using Microsoft.Analytics.Interfaces;
-using Microsoft.Analytics.Interfaces.Streaming;
-using Microsoft.Analytics.Types.Sql;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
+using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Types.Sql;
 
 namespace CustomMayd.DataLake.Extractors
 {
     public class FixedWidthExtractor : IExtractor
     {
+        private readonly SqlMap<string, string> _colWidths;
         private readonly Encoding _encoding;
         private readonly byte[] _rowDelim;
-        private readonly SqlMap<string, string> _colWidths;
 
         public FixedWidthExtractor(SqlMap<string, string> colWidths, Encoding encoding = null, string rowDelim = "\r\n")
         {
@@ -30,43 +28,42 @@ namespace CustomMayd.DataLake.Extractors
 
         public override IEnumerable<IRow> Extract(IUnstructuredReader input, IUpdatableRow output)
         {
-            foreach (Stream currentLine in input.Split(_rowDelim))
-            {
-                using (StreamReader lineReader = new StreamReader(currentLine, _encoding))
+            foreach (var currentLine in input.Split(_rowDelim))
+                using (var lineReader = new StreamReader(currentLine, _encoding))
                 {
-                    string line = lineReader.ReadToEnd();
+                    var line = lineReader.ReadToEnd();
                     //read new line of input
-                    int startParse = 0;
+                    var startParse = 0;
 
                     //for each column
-                    int i = 0;
+                    var i = 0;
                     foreach (var colWidth in _colWidths)
                     {
                         //read chars associated with fixed-width column
-                        int charsToRead = int.Parse(colWidth.Value);
-                        string value = line.Substring(startParse, charsToRead);
+                        var charsToRead = int.Parse(colWidth.Value);
+                        var value = line.Substring(startParse, charsToRead);
 
 
                         //assign value to output (w/ appropriate type)
                         switch (output.Schema[i].Type.Name)
                         {
-                            case ("String"):
+                            case "String":
                                 output.Set(i, value);
                                 break;
-                            case ("Int32"):
+                            case "Int32":
                                 output.Set(i, int.Parse(value));
                                 break;
-                            case ("Double"):
+                            case "Double":
                                 output.Set(i, double.Parse(value));
                                 break;
-                            case ("Float"):
+                            case "Float":
                                 output.Set(i, float.Parse(value));
                                 break;
-                            case ("DateTime"):
+                            case "DateTime":
                                 output.Set(i, DateTime.Parse(value));
                                 break;
                             default:
-                                throw (new Exception("Unknown data type specified: " + output.Schema[i].Type.Name));
+                                throw new Exception("Unknown data type specified: " + output.Schema[i].Type.Name);
                         }
 
                         //move to start of next column
@@ -74,11 +71,9 @@ namespace CustomMayd.DataLake.Extractors
                         i++;
                     }
 
-
                     //send output
                     yield return output.AsReadOnly();
                 }
-            }
         }
     }
 }
